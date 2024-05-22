4a9cac387e6072a0ba7547a366cae4dab6538b73
Enzodtz <enzodtz@gmail.com>
2024-05-21 23:57:54 -0300

    Add folding, add mapping

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,5 +1,28 @@
+require "custom.library.highlighted_foldtext"
+
+local vim = vim
+local opt = vim.opt
+
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldtext=[[luaeval('HighlightedFoldtext')()]]
+opt.fillchars="fold: "
+-- opt.foldtext = 'lua.vim.treeesitter.foldtext()'
+opt.foldenable = false
+
 ---@type ChadrcConfig 
- local M = {}
- M.ui = {theme = 'radium'}
+ local M = {
+  mappings = {
+    ["utils"] = {
+      n = {
+        ["<A-m>"] = {
+          ":%s/\r$<CR>",
+          "Remove ^m characters from buffer"
+        }
+      }
+    }
+  }
+}
+ M.ui = {theme = 'oxocarbon'}
  M.plugins = "custom.plugins"
  return M
