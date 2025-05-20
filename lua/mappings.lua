7e13c512844030e2cc779141ad9e0a63da99a5ef
Enzodtz <enzodtz@gmail.com>
2025-05-19 21:49:29 -0300

    Add lsp diag hover

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<leader>e", function()
+  vim.diagnostic.open_float { border = "rounded" }
+end, { desc = "Floating diagnostic" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
